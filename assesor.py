0e5479a44dabfd668093ee2f6ebb8e4159261ade
Donovan <d.bidlack@cue.me>
2022-01-27 06:44:26 -0700

    completed assesor will expand test cases

--- assesor.py
+++ assesor.py
@@ -1,15 +1,37 @@
 import os
+import subprocess
 import argparse
 import tkinter as tk
 from tkinter import filedialog
-import datetime as dt
+from datetime import datetime as dt
+import filecmp as fc
+import itertools as it
+import binaryCreator
 
 EXE = None
+CLEAN = False
 
-def getExe():
+def cleanup():
+    test_names = open('test_names.txt', 'r')
+    for test_name in test_names:
+        if test_name != "":
+            if os.path.exists(test_name[0:-1] + ".txt"):
+                os.remove(test_name[0:-1] + ".txt")
+            if os.path.exists(test_name[0:-1] + ".bin"):
+                os.remove(test_name[0:-1] + ".bin")
+            if os.path.exists(test_name[0:-1] + "_submission.txt"):
+                os.remove(test_name[0:-1] + "_submission.txt")
+    test_names.close()
+
+def get_exe():
+    global EXE
+    global CLEAN
     parser = argparse.ArgumentParser(description='Assesses the assesment submitted for employment.')
     parser.add_argument('-e', '--executable', type=str, required=False, help="Executable submission.")
+    parser.add_argument('-d', action='store_true', help="Delete all created files.")
     args = parser.parse_args()
+    if args.d:
+        CLEAN = True
     if args.executable:
         EXE = args.executable
     else:
@@ -18,9 +40,67 @@ def getExe():
         root.withdraw()
         EXE = filedialog.askopenfilename()
 
-def main():
-    getExe()
+def run_script(args:list) -> list:
+    ret = [ 0, "no error"]
+    try: 
+        ret[0] = subprocess.check_call(args)
+    except subprocess.CalledProcessError as e:
+        ret[1] = e.output
+    return ret
 
+def print_txts(test_name:str):
+    submission = open(test_name + '_submission.txt', 'r')
+    correct = open(test_name + '.txt', 'r')
+    print("**SUBMISSION**              **CORRECT_OUTPUT**      ")
+    for submission_line, correct_line in it.zip_longest(submission, correct):
+        if submission_line is None:
+            print(f'{" ":<24}' + "    " + f'{correct_line[0:-1]:<24}')
+        elif correct_line is None:
+            print(f'{submission_line[0:-1]:<24}' + "    " + f'{" ":<24}')
+        else:
+            print(f'{submission_line[0:-1]:<24}' + "    " + f'{correct_line[0:-1]:<24}')
+    submission.close()
+    correct.close()
+
+def run_test(test_name:str):
+    args = [EXE, test_name + ".bin", test_name + "_submission.txt"]
+    start_time = dt.now()
+    returned = run_script(args)
+    if returned[1] != "no error":
+        print(test_name + " hard faulted.")
+        print(returned[0])
+        print(returned[1])
+        print()
+        print()
+    else:
+        end_time = dt.now()
+        time_delta = end_time - start_time
+        if fc.cmp(test_name + ".txt", test_name + "_submission.txt", shallow = False):
+            print(test_name + " passed in " + str(time_delta) + " seconds.")
+            print()
+            print()
+        else:
+            print(test_name + " failed in " + str(time_delta) + " seconds.")
+            print_txts(test_name)
+            print()
+            print()
+
+def main():
+    get_exe()
+    if os.path.exists("test_names.txt"):
+        test_names = open('test_names.txt', 'r')
+        print()
+        for test_name in test_names:
+            if test_name != "":
+                run_test(test_name[0:-1])
+        test_names.close()
+        if CLEAN:
+            cleanup()
+        os.remove("test_names.txt")
+    else:
+        print("Must run binaryCreatorFirst.")
+
+    
 
 if __name__ == "__main__":
     main()
--- binaryCreator.py
+++ binaryCreator.py
@@ -1,42 +1,57 @@
 import argparse
 import random as ra
 
+from numpy import number
+
 MAX_VALUE = (2**12)-1
 
+def add_testname(test_name:str):
+    file = open("test_names.txt", "a")
+    file.write(test_name + "\n")
+    file.close()
+
 def create_binary_file(filename:str, vars:list):
+    add_testname(filename)
     file = open(filename + ".bin", "wb")
     size = len(vars)
     i = 0
     while i < size:
-        binary = '{0:012b}'.format(vars[i]) + '{0:012b}'.format(vars[i+1])
-        triple_byte = []
-        triple_byte.append(int(binary[0:8], 2))
-        triple_byte.append(int(binary[8:16], 2))
-        triple_byte.append(int(binary[16:24], 2))
-        file.write(bytearray(triple_byte))
+        if size == 1:
+            file.write(bytearray(vars))
+        else:
+            binary = '{0:012b}'.format(vars[i]) + '{0:012b}'.format(vars[i+1])
+            bytes_to_write = []
+            bytes_to_write.append(int(binary[0:8], 2))
+            bytes_to_write.append(int(binary[8:16], 2))
+            bytes_to_write.append(int(binary[16:24], 2))
+            file.write(bytearray(bytes_to_write))
         i+=2
     file.close()
 
 def create_text_file(filename:str, vars:list):
     file = open(filename + ".txt", "w")
-    if vars[-1] == 0:
-        vars.pop()
-    size = len(vars)
-    sorted_vars = sorted(vars, reverse=True)
-    if size > 32:
-        size = 32
-    i = 1
-    file.write('--Sorted Max 32 Values--\n')
-    while i <= size:
-        file.write(str(sorted_vars[-i]))
-        file.write("\n")
-        i+=1
-    file.write('--Last 32 Values--\n')
-    i = size
-    while i > 0 :
-        file.write(str(vars[-i]))
-        file.write('\n')
-        i-=1
+    if len(vars) == 0:
+        file.write('--Sorted Max 32 Values--\n')
+        file.write('--Last 32 Values--\n')
+    else:
+        if vars[-1] == 0:
+            vars.pop()
+        size = len(vars)
+        sorted_vars = sorted(vars, reverse=False)
+        if size > 32:
+            size = 32
+        i = size
+        file.write('--Sorted Max 32 Values--\n')
+        while i > 0:
+            file.write(str(sorted_vars[-i]))
+            file.write("\n")
+            i-=1
+        file.write('--Last 32 Values--\n')
+        i = size
+        while i > 0 :
+            file.write(str(vars[-i]))
+            file.write('\n')
+            i-=1
     file.close()
 
 def simple_test():
@@ -51,10 +66,41 @@ def zero_at_end():
     create_binary_file(filename, vars)
     create_text_file(filename, vars)
 
+def empty_file():
+    filename = "empty_file"
+    vars = [ ]
+    create_binary_file(filename, vars)
+    create_text_file(filename, vars)
+
+def one_byte():
+    filename = "one_byte"
+    vars = [ 33 ]
+    create_binary_file(filename, vars)
+    create_text_file(filename, vars)
+
+def fifty_vars():
+    filename = "fifty_vars"
+    value = 358
+    vars = [ ]
+    number_of_vars = 50
+    i = 0
+    while i < number_of_vars:
+        value = value * i - 52
+        while value < 0:
+            value += MAX_VALUE
+        while value > MAX_VALUE:
+            value -= MAX_VALUE
+        vars.append(value)
+        i+=1
+    create_binary_file(filename, vars)
+    create_text_file(filename, vars)
+
 def main():
-    parser = argparse.ArgumentParser(description='Creates binary files with randomish variables that are a 12 bits wide.')
     simple_test()
     zero_at_end()
+    empty_file()
+    one_byte()
+    fifty_vars()
 
 if __name__ == "__main__":
     main()
